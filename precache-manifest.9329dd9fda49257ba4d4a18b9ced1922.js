1101980c914189c6376f9c6e5b1a24759857b139
Lars Karbo <mail@larskarbo.no>
2019-11-24 12:55:42 +0100

    Updates

---
+++ precache-manifest.9329dd9fda49257ba4d4a18b9ced1922.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9dd9ea506ed2ccf0aefcc32ba27afb66",
+    "url": "/2020/index.html"
+  },
+  {
+    "revision": "af65d980390699ef204d",
+    "url": "/2020/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "d6a2791502024fa25479",
+    "url": "/2020/static/js/2.dd4c0ef5.chunk.js"
+  },
+  {
+    "revision": "af65d980390699ef204d",
+    "url": "/2020/static/js/main.4aea8f26.chunk.js"
+  },
+  {
+    "revision": "05bf51f17733a18d8d5d",
+    "url": "/2020/static/js/runtime-main.37f5d3d0.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/2020/static/media/logo.25bf045c.svg"
+  }
+]);
